8b94e7ae70c29d5c5aa8dd67de1d95fcedeb68f7
dani <dani.zaghian@gmail.com>
2021-01-08 12:07:54 -0800

    removes key

--- weather.rb
+++ weather.rb
@@ -5,7 +5,7 @@ puts "Welcome to the awesome Weather App!"
 puts "Type the name of a city:"
 city = gets.chomp
 
-response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=#{city}&units=imperial&APPID=9beb296210c3171be00be7e99d0e7ba8")
+response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=#{city}&units=imperial&APPID=YOURAPIKEY")
 
 weather_data = response.parse
 
